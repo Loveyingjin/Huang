cd3bdb38d5478c3d2c45f046c08bba0ed11630b1
Loveyingjin <147062684+Loveyingjin@users.noreply.github.com>
2023-10-26 22:42:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,19 +10,11 @@ func main() {
 	args := os.Args
 	pushPlusToken := args[1]
 	refreshTokens := args[2]
-	bilibiliCookies := args[3]
 	if refreshTokens != "null" {
 		refreshTokenList := strings.Split(refreshTokens, ",")
 		aliCloudDisk := platform.AliCloudDisk{}
 		for _, refreshToken := range refreshTokenList {
 			aliCloudDisk.Run(pushPlusToken, refreshToken)
-		}
-	}
-	if bilibiliCookies != "null" {
-		bilibiliCookieList := strings.Split(bilibiliCookies, ",")
-		bilibili := platform.Bilibili{}
-		for _, bilibiliCookie := range bilibiliCookieList {
-			bilibili.Run(pushPlusToken, bilibiliCookie)
 		}
 	}
 
